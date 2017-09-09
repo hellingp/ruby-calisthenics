aa4a469bb8fc20d96ce4dde26f1e432c84cf0938
Paul Helling <hellingpg@gmail.com>
2017-09-09 16:59:04 +0000

    Finished writing code for dessert.rb

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,34 @@
+# Create a class called Dessert
 class Dessert
-  def initialize(name, calories)
-    # your code here
-  end
-  def healthy?
-    # your code here
-  end
-  def delicious?
-    # your code here
-  end
+    def initialize(name, calories)
+        @name = name
+        @calories = calories
+    end
+    attr_accessor :name
+    attr_accessor :calories
+    def healthy?
+        if @calories < 200
+            return true
+        end
+        return false
+    end
+    def delicious?
+        return true
+    end
 end
 
 class JellyBean < Dessert
-  def initialize(flavor)
-    # your code here
-  end
+    def initialize(flavor)
+        @flavor = flavor
+        @name = flavor + " jelly bean"
+        @calories = 5
+    end
+    def delicious?
+        super
+        if @flavor != "licorice"
+            return true
+        else
+            return false
+        end
+    end
 end
