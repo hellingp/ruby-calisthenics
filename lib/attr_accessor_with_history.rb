b2625aaa8f21f86efab61d30831f8fb1e866b7a4
Paul Helling <hellingpg@gmail.com>
2017-09-16 01:47:28 +0000

    Second commit. Started working on fun_with_strings.rb and finished attr_accessor_with_history.rb

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -4,7 +4,33 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(value)
+        if !defined? @#{attr_name}_history
+          puts "first time"
+          @#{attr_name}_history = [@#{attr_name}]
+        else
+          @#{attr_name}_history << @#{attr_name}
+        end
+        @#{attr_name} = value
+      end
     }
   end
+end
+
+class Foo 
+  attr_accessor_with_history :bar
+end
+
+class Main
+  f = Foo.new
+  puts "history should be 1 nil"
+  puts f.bar_history
+  f.bar = 'x'
+  puts "history should still be 1 nil"
+  puts f.bar_history
+  f.bar = 'y'
+  puts "history should be nill, x"
+  puts f.bar_history
+  f.bar = 'z'
+  puts f.bar_history
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,42 @@
 module FunWithStrings
+  # Check if a string is a palindrome. Returns true or false
   def palindrome?
-    # your code here
+    # Ignore case
+    forward = self.downcase
+    # Ignore non alphabetic characters
+    forward = forward.gsub(/[^a-z]/, '')
+    # Reverse string
+    backward = forward.reverse
+    # Check if it's a palindrome (return is implied)
+    backward == forward
   end
+  # Counts words. Returns a hash.
   def count_words
-    # your code here
+    # Should return a hash
+    count = Hash.new(0)
+    # Split string
+    words = self.downcase.gsub(/[^a-z]/, ' ').split(' ')
+    # Loop through words
+    words.each do |word|
+      # Add 1 to count
+      count[word] += 1
+    end
+    return count
   end
   def anagram_groups
-    # your code here
+    # Create an array for the results
+    results = Array.new
+    # Create a hash to count words
+    counts = Hash(Array.new)
+    # Split string
+    words = self.split(' ')
+    words.each do |word|
+      # Create an array of sorted chars ignoring case
+      scramble = word.downcase.chars.sort
+      # Add a word to scramble in the hash
+      counts[scramble.push(word)]
+    end
+    return counts
   end
 end
 
